eee4b6625f2c3eaa8952ae2886c01c0f33564b54
Min Lwin <lwin.zawmin@gmail.com>
2024-09-15 07:58:40 +0630

    Share Project

---
+++ src/main/java/com/jdc/commons/CommonsConfiguration.java
@@ -0,0 +1,8 @@
+package com.jdc.commons;
+
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class CommonsConfiguration {
+
+}
---
+++ src/main/java/com/jdc/commons/services/HelloService.java
@@ -0,0 +1,11 @@
+package com.jdc.commons.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+	public String message() {
+		return "Hello Maven Package Work flow.";
+	}
+}
